c77aab79c3ab4dcd0ff909d486dfff575b44e0ae
ton1czech <tonyasek007@gmail.com>
2023-03-12 16:34:18 +0100

    🎁NEW: comments and actually run git commit

--- fastmitts.sh
+++ fastmitts.sh
@@ -1,13 +1,18 @@
 #!/bin/sh
 
+# new line character
 NL=$'\n'
 
+# choose from possible options
 CHOOSE=$(gum choose \
     "🎁NEW:" \
     "👌IMPROVE:" \
     "🐛BUG FIX:" \
     "❌REMOVED:" \
     "🚀STABLE RELEASE:")
+
+# get user's commit message
 TEXT=$(gum input --placeholder "text")      
 
-gum confirm "$CHOOSE $TEXT $NL$NL commit?" && echo "git commit -m $CHOOSE $TEXT"
+# show commit message and ask for confirmation then run git commit
+gum confirm "$CHOOSE $TEXT $NL$NL commit?" && git commit -m "$CHOOSE $TEXT"
